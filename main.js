cfaa6d8f0fb81b98ed8ceba56dcbb4577d60bbbf
AarushSarda <77834111+AarushSarda@users.noreply.github.com>
2021-08-31 18:30:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(500,500);
+    canvas = createCanvas(550,400);
+    canvas.position (560,150);
+    posenet = ml5.poseNet(video , modelLoaded);
+    posenet.on('pose', gotPoses);
+}
+function draw() {
+    background("#200cf7");
+}
+function modelLoaded() {
+    console.log("PoseNet is Loaded")
+}
+function gotPoses(results) {
+    if (results.length > 0){
+        console.log(results);
+    }
+}
